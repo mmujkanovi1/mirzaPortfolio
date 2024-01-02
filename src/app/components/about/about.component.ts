e010f0110da73a94533b198a7c46f75ad19cad7e
mirza <mirza@olivebh.com>
2024-01-02 22:03:35 +0100

    edit styles - 2

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var anime: any;
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -9,16 +11,39 @@ export class AboutComponent implements OnInit {
   visible: boolean = false;
 
   showDialog() {
-      this.visible = true;
+    this.visible = true;
   }
 
-  panelTitle= "panel title";
-  panelContent= "panel content"
+  panelTitle = "panel title";
+  panelContent = "panel content"
 
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
-}
+      var textWrapper = document.querySelector('.ml2');
+      textWrapper!.innerHTML = textWrapper!.textContent!.replace(/\S/g, "<span class='letter'>$&</span>");
+
+      anime.timeline({ loop: true })
+        .add({
+          targets: '.ml2 .letter',
+          scale: [4, 1],
+          opacity: [0, 1],
+          translateZ: 0,
+          easing: "easeOutExpo",
+          duration: 950,
+          delay: (el: number, i: number) => 70 * i
+        }).add({
+          targets: '.ml2',
+          opacity: 0,
+          duration: 1000,
+          easing: "easeOutExpo",
+          delay: 1000
+        });
+
+    }
+
+
+
+  }
